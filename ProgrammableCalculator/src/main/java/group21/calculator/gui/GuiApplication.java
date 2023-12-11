3fd8d14d26dd2eb1382e811ba885a62de9c2c256
nelloodeluca <nelloodeluca@gmail.com>
2023-12-11 13:56:18 +0100

    Icon + Comment Testing

--- ProgrammableCalculator/src/main/java/group21/calculator/gui/GuiApplication.java
+++ ProgrammableCalculator/src/main/java/group21/calculator/gui/GuiApplication.java
@@ -3,6 +3,7 @@ package group21.calculator.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class GuiApplication extends Application  {
         FXMLLoader fxmlLoader = new FXMLLoader(GuiApplication.class.getResource("gui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("ProgrammableCalculator");
+        //stage.getIcons().add(new Image("src/main/resources/images/icon.png"));
         stage.setScene(scene);
         stage.show();
     }
--- ProgrammableCalculator/src/test/java/group21/calculator/test/ComplexNumberTest.java
+++ ProgrammableCalculator/src/test/java/group21/calculator/test/ComplexNumberTest.java
@@ -34,6 +34,9 @@ class ComplexNumberTest {
         assertEquals(3, n.getReal());
     }
 
+    /**
+     * Test of getImaginary method, of class ComplexNumber.
+     */
     @Test
     void testGetImaginary() {
         System.out.println("Testing getImaginary");
@@ -41,6 +44,9 @@ class ComplexNumberTest {
         assertEquals(4, n.getImaginary());
     }
 
+    /**
+     * Test of add method, of class ComplexNumber.
+     */
     @Test
     void testAdd() {
         ComplexNumber n = new ComplexNumber(3,4);
@@ -48,6 +54,9 @@ class ComplexNumberTest {
         assertTrue(out.getReal() == 5 && out.getImaginary() == 7);
     }
 
+    /**
+     * Test of subtract method, of class ComplexNumber.
+     */
     @Test
     void testSubtract() {
         ComplexNumber n = new ComplexNumber(3,4);
@@ -55,13 +64,18 @@ class ComplexNumberTest {
         assertTrue(out.getReal() == 1 && out.getImaginary() == 1);
     }
 
+    /**
+     * Test of multiply method, of class ComplexNumber.
+     */
     @Test
     void testMultiply() {
         ComplexNumber n = new ComplexNumber(3,4);
         ComplexNumber out = n.multiply(other);
         assertTrue(out.getReal() == -6 && out.getImaginary() == 17);
     }
-
+    /**
+     * Test of divide method, of class ComplexNumber.
+     */
     @Test
     void testDivide() {
         ComplexNumber n = new ComplexNumber(12,3);
@@ -69,6 +83,9 @@ class ComplexNumberTest {
         assertTrue(out.getReal() == 3.9 && out.getImaginary() == -0.3);
     }
 
+    /**
+     * Test of squareRoot method, of class ComplexNumber.
+     */
     @Test
     void testSquareRoot() {
         ComplexNumber n = new ComplexNumber(3,4);
@@ -76,6 +93,9 @@ class ComplexNumberTest {
         assertTrue(out.getReal() == 2 && out.getImaginary() == 1);
     }
 
+    /**
+     * Test of invertSign method, of class ComplexNumber.
+     */
     @Test
     void testInvertSign() {
         ComplexNumber n = new ComplexNumber(3,-4);
@@ -83,39 +103,42 @@ class ComplexNumberTest {
         assertTrue(out.getReal() == -3 && out.getImaginary() == 4);
     }
 
+    /**
+     * Test of complexParse method, of class ComplexNumber.
+     */
     @Test
     void testComplexParse0() {
-        ComplexNumber n = ComplexNumber.complexParse("3+4j");// -3+4j, -3-4j, -3 , 4j, -4j, 0+4j
+        ComplexNumber n = ComplexNumber.complexParse("3+4j");
         assertTrue(n.getReal() == 3 && n.getImaginary() == 4);
     }
 
     @Test
     void testComplexParse1() {
-        ComplexNumber n = ComplexNumber.complexParse("-3+4j");// -3+4j, -3-4j, -3 , 4j, -4j, 0+4j
+        ComplexNumber n = ComplexNumber.complexParse("-3+4j");
         assertTrue(n.getReal() == -3 && n.getImaginary() == 4);
     }
 
     @Test
     void testComplexParse2() {
-        ComplexNumber n = ComplexNumber.complexParse("3-4j");// -3+4j, -3-4j, -3 , 4j, -4j, 0+4j
+        ComplexNumber n = ComplexNumber.complexParse("3-4j");
         assertTrue(n.getReal() == 3 && n.getImaginary() == -4);
     }
 
     @Test
     void testComplexParse3() {
-        ComplexNumber n = ComplexNumber.complexParse("0+4j");// -3+4j, -3-4j, -3 , 4j, -4j, 0+4j
+        ComplexNumber n = ComplexNumber.complexParse("0+4j");
         assertTrue(n.getReal() == 0 && n.getImaginary() == 4);
     }
 
     @Test
     void testComplexParse4() {
-        ComplexNumber n = ComplexNumber.complexParse("3");// -3+4j, -3-4j, -3 , 4j, -4j, 0+4j
+        ComplexNumber n = ComplexNumber.complexParse("3");
         assertTrue(n.getReal() == 3 && n.getImaginary() == 0);
     }
 
     @Test
     void testComplexParse5() {
-        ComplexNumber n = ComplexNumber.complexParse("-4j");// -3+4j, -3-4j, -3 , 4j, -4j, 0+4j
+        ComplexNumber n = ComplexNumber.complexParse("-4j");
         assertTrue(n.getReal() == 0 && n.getImaginary() == -4);
     }
 
