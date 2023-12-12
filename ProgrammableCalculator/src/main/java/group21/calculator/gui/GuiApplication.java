277b4daee9ab210790af83c822128ee38d49a0b6
nelloodeluca <nelloodeluca@gmail.com>
2023-12-12 21:50:45 +0100

    added icon + css + changes about gui interface

--- ProgrammableCalculator/src/main/java/group21/calculator/gui/GuiApplication.java
+++ ProgrammableCalculator/src/main/java/group21/calculator/gui/GuiApplication.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 
+
 public class GuiApplication extends Application  {
 
     @Override
@@ -16,8 +17,8 @@ public class GuiApplication extends Application  {
         FXMLLoader fxmlLoader = new FXMLLoader(GuiApplication.class.getResource("gui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("ProgrammableCalculator");
-
-        stage.getIcons().add(new Image("https://cdn0.iconfinder.com/data/icons/font-awesome-solid-vol-1/512/calculator-512.png"));
+        stage.setResizable(false);
+        stage.getIcons().add(new Image("icon.png"));
 
         stage.setScene(scene);
         stage.show();
